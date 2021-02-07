68205bf7cca30715ff4210927b5ab8bd2f320878
nursm86 <lucifersm86@gmail.com>
2021-02-07 22:37:34 +0600

    p2

---
+++ Area Practice/Area Practice/Areas/Product/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Area_Practice.Areas.Product.Data;
+
+namespace Area_Practice.Areas.Product.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductRepository proRepo = new ProductRepository();
+        // GET: Product/Product
+        public ActionResult ProductList()
+        {
+            return View(proRepo.GetAll());
+        }
+
+        public ActionResult ProductAddEdit()
+        {
+            return View();
+        }
+    }
+}
---
+++ Area Practice/Area Practice/Areas/Product/Data/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Area_Practice.Models;
+using Area_Practice.Repositories;
+
+namespace Area_Practice.Areas.Product.Data
+{
+    public class ProductRepository : Repository<product>
+    {
+        
+    }
+}
